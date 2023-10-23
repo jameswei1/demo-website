6cbb66279e6565f4a1e8b5b76d52939e81685c8f
JAMES WEI <j65wei@uwaterloo.ca>
2023-10-23 12:25:25 -0400

    working axios.get, render of list

---
+++ src/PayeeList.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useState } from 'react';
+
+export default function PayeeList({ payees }) {
+    const [count, setCount] = useState(0);
+    const [checked, setChecked] = useState([0]);
+
+    function handleToggle() {
+        console.log("Toggled")
+    }
+
+    payees = ["asfd", "sdfg", "sdfg"]
+
+    return (
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: '#cfe8fc' }}>
+            {payees.map(p => {
+                <div key={p}>{p}</div>
+            })}
+        </List>
+    )
+}
+
+
+            // let curr = count
+            // setCount(curr + 1)
+            // let value = curr
+            // let labelId = `checkbox-list-label-${value}`;
+            // return (
+            //     <ListItem
+            //         key={value}
+            //         secondaryAction={
+            //         <IconButton edge="end" aria-label="comments">
+            //             <DeleteIcon />
+            //         </IconButton>
+            //         }
+            //         disablePadding
+            //     >
+            //         <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+            //         <ListItemIcon>
+            //             <Checkbox
+            //             edge="start"
+            //             checked={checked.indexOf(value) !== -1}
+            //             tabIndex={-1}
+            //             disableRipple
+            //             inputProps={{ 'aria-labelledby': labelId }}
+            //             />
+            //         </ListItemIcon>
+            //         <ListItemText id={labelId} primary={p} />
+            //         </ListItemButton>
+            //     </ListItem>
+            // )
