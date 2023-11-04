00407ec7dd9ff706f912518dcc61fda81c18ef2d
JAMES WEI <j65wei@uwaterloo.ca>
2023-11-04 11:52:56 -0400

    test minor change

--- src/Ribbon.js
+++ src/Ribbon.js
@@ -8,7 +8,7 @@ export default function Ribbon() {
         <AppBar position='static'>
             <Toolbar display="flex" justifyContent="flex-end">
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} paddingLeft={2}>
-                    James Wei - Flipp Takehome Assessment
+                    James Wei - Flipp Takehome Assessment Edited
                 </Typography>
                 <Button variant='contained'>Logout</Button>
             </Toolbar>
