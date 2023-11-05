c6720093a979be832a6d703e557c8991e8d06737
JAMES WEI <j65wei@uwaterloo.ca>
2023-11-04 21:31:58 -0400

    Host Public IP displays

--- src/Ribbon.js
+++ src/Ribbon.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import './Ribbon.css';
 import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
+import axios from 'axios';
 
 export default function Ribbon() {
+  const [publicIp, setPublicIP] = useState("")
+  useEffect(() => {
+    axios
+    .get('https://api.ipify.org?format=json')
+    .then((res) => {
+      setPublicIP(res.data.ip)
+    })
+  }, [])
+
   return (
     <Box display="flex" justifyContent="flex-end" alignItems="flex-end" >
         <AppBar position='static'>
             <Toolbar display="flex" justifyContent="flex-end">
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} paddingLeft={2}>
-                    James Wei - Flipp Takehome Assessment Edited
+                    James Wei - Flipp Takehome Assessment - Host's Public IP Address: {publicIp}
                 </Typography>
                 <Button variant='contained'>Logout</Button>
             </Toolbar>
